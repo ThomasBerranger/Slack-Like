998bfd16df9ca8173c651e0091522ff6f8704b03
ThomasBerranger <tberranger@hotmail.fr>
2018-01-02 21:10:42 +0100

    Some fixed bug and try made

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var methodOverride = require('method-override');
 var expressSession = require('express-session');
-var moment = require('moment');
 
 var User = require('./models/user');
 var Post = require('./models/post');
@@ -20,12 +19,12 @@ var blog = require('./routes/blog');
 
 var app = express();
 
-/*  @NOTE
- *  DeprecationWarning: `open()` is deprecated in mongoose >= 4.11.0, use
- *  `openUri()` instead, or set the `useMongoClient` option if using
- *  `connect()` or `createConnection()`.
- */
+var server = require('http').createServer(app);  
+var io = require('socket.io')(server);
+
+
 mongoose.Promise = global.Promise;
+
 let database = mongoose.connect('mongodb://localhost/slack-like', {
     useMongoClient: true,
     socketTimeoutMS: 0,
@@ -34,7 +33,7 @@ let database = mongoose.connect('mongodb://localhost/slack-like', {
 });
 
 database.then(function (db) {
-    console.log('DB : Ready to work !');
+    console.log('DataBase : Ready to work !');
 });
 
 //- Method override
@@ -57,35 +56,39 @@ passport.deserializeUser(function(id, done){
   });
 });
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/auth', auth);
 app.use('/blog', blog);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+io.on('connection', function(client) {  
+  console.log('Client connected...');
+  /* Bloqué :'( */
+});
+
 
 module.exports = app;
